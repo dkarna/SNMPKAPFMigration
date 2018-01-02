751d3a70c8637515498e11a2ec487d2573ce77b8
dkarna <karna.deepak@gmail.com>
2018-01-02 20:06:35 +0545

    Added IDCUPL.sql in TF

---
+++ SQLScripts/ImportQueries/TradeFinance/IDCUPL.sql
@@ -0,0 +1,4 @@
+Dear sir,
+
+I would like to inform you that I am no more interested in continuing as a PL in Computing department at NAMI, so I would like to resign from this post. I will support as a PL for this term of Level 5 and 6 as it may affect the academics. Please search for a good PL so that I can hand over the things smoothly and won't effect the further sessions.
+

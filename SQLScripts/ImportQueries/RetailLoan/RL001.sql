ebb9cefed8cbb48f632560defd5edd898018e5ce
dkarna <karna.deepak@gmail.com>
2018-04-04 18:59:36 +0545

    added modified mapping sheet and sql scripts

--- SQLScripts/ImportQueries/RetailLoan/RL001.sql
+++ SQLScripts/ImportQueries/RetailLoan/RL001.sql
@@ -34,6 +34,59 @@ LEFT JOIN AcCustType ad
   AND ad.CustTypeCode = 'R'
 WHERE fn.CustTypeCode IN ('C','X','F','P','R') ORDER by 1,2
 
+-- Temporary FinacleLoanTable with various logics for LAA, ODA and PCA type of loans
+
+select t.* into #tempFinacleLoanTable from 
+(
+	
+-- LAA
+
+	-- Without AcType 36
+
+	select flt.* from FinacleLoanTable flt
+	join Master m on m.MainCode=flt.PumoriMainCode and m.AcType=flt.PumoriAcType and m.CyCode=flt.PumoriCyCode
+	where flt.FinacleSchemeType='LAA'
+	and (m.Balance < 0 and m.Limit > 0)
+	and m.AcType IN('30','31','32','33','37','38','39','3A','3B','3E','3F','3K','3L','3M','3N','3O','3P','3Q','3R','3S','3T','3U','3X','40','42','43','46','4A','4B','4F','4G','4I','4J','4K','4M','4O','4P','4Q','4S','3V','3Y','3Z','47','48','4C','4D','4E')
+
+	-- AcType 36 only
+	UNION
+	select flt.* from FinacleLoanTable flt
+	join Master m on m.MainCode=flt.PumoriMainCode and m.AcType=flt.PumoriAcType and m.CyCode=flt.PumoriCyCode
+	where flt.FinacleSchemeType='LAA'
+	and (m.Balance < 0)
+	and m.AcType IN('36')
+
+	--select * from FinacleLoanTable where FinacleSchemeType='LAA' 
+
+	--select top 1 * from Master where AcType='36'
+
+
+
+-- ODA
+	
+	UNION
+
+	select flt.* from FinacleLoanTable flt
+	join Master m on m.MainCode=flt.PumoriMainCode and m.AcType=flt.PumoriAcType and m.CyCode=flt.PumoriCyCode
+	where flt.FinacleSchemeType='ODA'
+	and m.IsBlocked not in ('C','o')
+	and m.AcType IN('30','31','32','33','37','38','39','3A','3B','3E','3F','3K','3L','3M','3N','3O','3P','3Q','3R','3S','3T','3U','3X','40','42','43','46','4A','4B','4F','4G','4I','4J','4K','4M','4O','4P','4Q','4S','3V','3Y','3Z','47','48','4C','4D','4E')
+
+
+-- PCA
+	--select top 1 * from DealTable
+	UNION
+	select flt.* from FinacleLoanTable flt
+	join Master m on m.MainCode=flt.PumoriMainCode and m.AcType=flt.PumoriAcType and m.CyCode=flt.PumoriCyCode
+	join DealTable dt on flt.PumoriReferenceNo=dt.ReferenceNo
+	where flt.FinacleSchemeType='PCA'
+	and dt.DealAmt > 0
+	--and (m.Balance < 0 and m.Limit > 0)
+	and m.AcType IN('30','31','32','33','37','38','39','3A','3B','3E','3F','3K','3L','3M','3N','3O','3P','3Q','3R','3S','3T','3U','3X','40','42','43','46','4A','4B','4F','4G','4I','4J','4K','4M','4O','4P','4Q','4S','3V','3Y','3Z','47','48','4C','4D','4E')
+) as t
+order by t.PumoriMainCode,t.PumoriAcType,t.PumoriCyCode
+
 
 
 SELECT * FROM
@@ -442,11 +495,11 @@ SELECT * FROM
 	) AS tmaster ON m.ClientCode = tmaster.ClientCode
 	LEFT JOIN #tempAcCustType tact ON tact.MainCode = lm.MainCode AND tact.BranchCode = lm.BranchCode
 	LEFT JOIN CurrencyTable curt ON m.CyCode = curt.CyCode
-	LEFT JOIN FinacleLoanTable flt ON m.MainCode = flt.PumoriMainCode AND m.AcType = flt.PumoriAcType AND m.CyCode = flt.PumoriCyCode -- use the join from master table
+	JOIN #tempFinacleLoanTable flt ON m.MainCode = flt.PumoriMainCode AND m.AcType = flt.PumoriAcType AND m.CyCode = flt.PumoriCyCode -- use the join from master table
 	LEFT JOIN PastDuedList pdl ON lm.MainCode = pdl.ReferenceNo AND lm.BranchCode = pdl.BranchCode
 	WHERE 1=1 
 	AND m.AcType IN('30','31','32','33','36','37','38','39','3A','3B','3E','3F','3K','3L','3M','3N','3O','3P','3Q','3R','3S','3T','3U','3X','40','42','43','46','4A','4B','4F','4G','4I','4J','4K','4M','4O','4P','4Q','4S','3V','3Y','3Z','47','48','4C','4D','4E')
-	AND m.Balance = 0 AND m.Limit = 0
+	AND m.Balance <> 0 AND m.Limit <> 0
 	AND LEN(m.ClientCode) >= 8
 	AND m.IsBlocked NOT IN ('C','o')
 	AND EXISTS
@@ -861,7 +914,7 @@ SELECT * FROM
 	) AS tmaster ON m.ClientCode = tmaster.ClientCode
 	LEFT JOIN #tempAcCustType tact ON tact.MainCode = lm.MainCode AND tact.BranchCode = lm.BranchCode
 	LEFT JOIN CurrencyTable curt ON m.CyCode = curt.CyCode
-	LEFT JOIN FinacleLoanTable flt ON m.MainCode = flt.PumoriMainCode AND m.AcType = flt.PumoriAcType AND m.CyCode = flt.PumoriCyCode -- use the join from master table
+	JOIN #tempFinacleLoanTable flt ON m.MainCode = flt.PumoriMainCode AND m.AcType = flt.PumoriAcType AND m.CyCode = flt.PumoriCyCode -- use the join from master table
 	LEFT JOIN PastDuedList pdl ON lm.MainCode = pdl.ReferenceNo AND lm.BranchCode = pdl.BranchCode
 	WHERE 1=1 
 	AND m.AcType IN('30','31','32','33','36','37','38','39','3A','3B','3E','3F','3K','3L','3M','3N','3O','3P','3Q','3R','3S','3T','3U','3X','40','42','43','46','4A','4B','4F','4G','4I','4J','4K','4M','4O','4P','4Q','4S','3V','3Y','3Z','47','48','4C','4D','4E')
---
+++ SQLScripts/ImportQueries/RetailLoan/working_20180404.sql
@@ -0,0 +1,49 @@
+select top 1 * from FinacleLoanTable
+select distinct FinacleSchemeType from FinacleLoanTable
+
+
+-- LAA
+
+	-- Without AcType 36
+
+	select flt.* from FinacleLoanTable flt
+	join Master m on m.MainCode=flt.PumoriMainCode and m.AcType=flt.PumoriAcType and m.CyCode=flt.PumoriCyCode
+	where flt.FinacleSchemeType='LAA'
+	and (m.Balance < 0 and m.Limit > 0)
+	and m.AcType IN('30','31','32','33','37','38','39','3A','3B','3E','3F','3K','3L','3M','3N','3O','3P','3Q','3R','3S','3T','3U','3X','40','42','43','46','4A','4B','4F','4G','4I','4J','4K','4M','4O','4P','4Q','4S','3V','3Y','3Z','47','48','4C','4D','4E')
+
+	-- AcType 36 only
+	UNION
+	select flt.* from FinacleLoanTable flt
+	join Master m on m.MainCode=flt.PumoriMainCode and m.AcType=flt.PumoriAcType and m.CyCode=flt.PumoriCyCode
+	where flt.FinacleSchemeType='LAA'
+	and (m.Balance < 0)
+	and m.AcType IN('36')
+
+	--select * from FinacleLoanTable where FinacleSchemeType='LAA' 
+
+	--select top 1 * from Master where AcType='36'
+
+
+
+-- ODA
+	
+	UNION
+
+	select flt.* from FinacleLoanTable flt
+	join Master m on m.MainCode=flt.PumoriMainCode and m.AcType=flt.PumoriAcType and m.CyCode=flt.PumoriCyCode
+	where flt.FinacleSchemeType='ODA'
+	and m.IsBlocked not in ('C','o')
+	and m.AcType IN('30','31','32','33','37','38','39','3A','3B','3E','3F','3K','3L','3M','3N','3O','3P','3Q','3R','3S','3T','3U','3X','40','42','43','46','4A','4B','4F','4G','4I','4J','4K','4M','4O','4P','4Q','4S','3V','3Y','3Z','47','48','4C','4D','4E')
+
+
+-- PCA
+	--select top 1 * from DealTable
+	UNION
+	select flt.* from FinacleLoanTable flt
+	join Master m on m.MainCode=flt.PumoriMainCode and m.AcType=flt.PumoriAcType and m.CyCode=flt.PumoriCyCode
+	join DealTable dt on flt.PumoriReferenceNo=dt.ReferenceNo
+	where flt.FinacleSchemeType='PCA'
+	and dt.DealAmt > 0
+	--and (m.Balance < 0 and m.Limit > 0)
+	and m.AcType IN('30','31','32','33','37','38','39','3A','3B','3E','3F','3K','3L','3M','3N','3O','3P','3Q','3R','3S','3T','3U','3X','40','42','43','46','4A','4B','4F','4G','4I','4J','4K','4M','4O','4P','4Q','4S','3V','3Y','3Z','47','48','4C','4D','4E')
